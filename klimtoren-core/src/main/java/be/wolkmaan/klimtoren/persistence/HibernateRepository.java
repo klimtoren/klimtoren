c9fea43bb004afb6f9ae1ea67d344efab20d17d4
karl <karl@192.168.0.156>
2014-05-15 09:10:19 +0200

    Get generic entity by id

--- klimtoren-core/src/main/java/be/wolkmaan/klimtoren/persistence/HibernateRepository.java
+++ klimtoren-core/src/main/java/be/wolkmaan/klimtoren/persistence/HibernateRepository.java
@@ -5,6 +5,8 @@
  */
 package be.wolkmaan.klimtoren.persistence;
 
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,4 +27,20 @@ public abstract class HibernateRepository<T> implements Repository<T> {
     public void store(Object entity) {
         getSession().saveOrUpdate(entity);
     }
+    @Override
+    public T get(Serializable id) {
+        return (T) getSession().get(genericClass(), id);
+    }
+    
+    
+    private Class<T> genericClass() {
+        ParameterizedType parameterizedType = (ParameterizedType) getClass()
+                .getGenericSuperclass();
+
+        @SuppressWarnings("unchecked")
+        Class<T> ret = (Class<T>) parameterizedType.getActualTypeArguments()[0];
+
+        return ret;
+    }
+
 }
