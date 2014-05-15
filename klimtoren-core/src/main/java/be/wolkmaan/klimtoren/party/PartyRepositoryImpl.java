9da7a57c057716d2f4e2812252f24656a160f426
karl <karl@192.168.0.156>
2014-05-15 09:09:39 +0200

    Get a relation by id

--- klimtoren-core/src/main/java/be/wolkmaan/klimtoren/party/PartyRepositoryImpl.java
+++ klimtoren-core/src/main/java/be/wolkmaan/klimtoren/party/PartyRepositoryImpl.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package be.wolkmaan.klimtoren.party;
 
 import be.wolkmaan.klimtoren.persistence.HibernateRepository;
-import org.hibernate.Criteria;
 import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 @Repository("partyRepository")
@@ -18,9 +17,16 @@ public class PartyRepositoryImpl extends HibernateRepository<Party> implements P
     public Person findByUsername(String username) {
         String qs = "select p.displayName, a.username from Person as p join p.authentication as a where a.username=:username";
         Query query = getSession().createQuery(qs)
-                        .setParameter("username", username);
+                .setParameter("username", username);
 
-        return (Person)query.uniqueResult();
+        return (Person) query.uniqueResult();
     }
-    
+
+    @Override
+    public PartyToPartyRelationship get(Long id) {
+        return (PartyToPartyRelationship)getSession().createCriteria(PartyToPartyRelationship.class)
+                    .add(Restrictions.eq("id", id))
+                    .uniqueResult();
+    }
+
 }
