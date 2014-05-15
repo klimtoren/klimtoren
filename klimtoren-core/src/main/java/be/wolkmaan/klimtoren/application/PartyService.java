4d5fd2f1dd187f83b2cc00f9f7ebc9da167cfe3a
karl <karl@192.168.0.156>
2014-05-15 09:13:00 +0200

    Register, start and stop relations

--- klimtoren-core/src/main/java/be/wolkmaan/klimtoren/application/PartyService.java
+++ klimtoren-core/src/main/java/be/wolkmaan/klimtoren/application/PartyService.java
@@ -6,9 +6,11 @@
 
 package be.wolkmaan.klimtoren.application;
 
+import be.wolkmaan.klimtoren.kind.Kind;
 import be.wolkmaan.klimtoren.party.Authentication;
 import be.wolkmaan.klimtoren.party.Organization;
 import be.wolkmaan.klimtoren.party.Party;
+import be.wolkmaan.klimtoren.party.PartyToPartyRelationship;
 import be.wolkmaan.klimtoren.party.Person;
 
 /**
@@ -53,4 +55,26 @@ public interface PartyService {
      * @return 
      */
     public Organization registerNewOrganization(String name, Party parent);
+    
+    /**
+     * Adds new relation between two parties
+     * @param context
+     * @param reference
+     * @param kind
+     * @return 
+     */
+    public PartyToPartyRelationship registerRelation(Party context, Party reference, Kind kind);
+    
+    /**
+     * Stops a relation between two parties.
+     * @param p2p
+     * @return 
+     */
+    public PartyToPartyRelationship stopRelation(PartyToPartyRelationship p2p);
+    /**
+     * Stops a relation between two parties. The relation is found by the identifier.
+     * @param id
+     * @return 
+     */
+    public PartyToPartyRelationship stopRelation(Long id);
 }
