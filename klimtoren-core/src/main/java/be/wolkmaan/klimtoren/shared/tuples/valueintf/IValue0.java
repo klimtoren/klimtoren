1bc0a9a4d4a688ae75d0086c4dcf6b1b623e0abe
karl <karl@10.0.1.171>
2014-05-23 15:39:43 +0200

    initial commit

---
+++ klimtoren-core/src/main/java/be/wolkmaan/klimtoren/shared/tuples/valueintf/IValue0.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package be.wolkmaan.klimtoren.shared.tuples.valueintf;
+
+/**
+ *
+ * @author karl
+ */
+public interface IValue0<T> {
+    public T getValue0();
+}
